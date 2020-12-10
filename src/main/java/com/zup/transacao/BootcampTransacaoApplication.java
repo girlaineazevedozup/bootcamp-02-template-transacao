f9df84a6c3b81a7fa232ac8cc2bf176958cf0e17
girlaine.azevedo <girlaine.azevedo@zup.com.br>
2020-12-10 09:56:11 -0300

    Commit inicial

---
+++ src/main/java/com/zup/transacao/BootcampTransacaoApplication.java
@@ -0,0 +1,13 @@
+package com.zup.transacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampTransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampTransacaoApplication.class, args);
+	}
+
+}
